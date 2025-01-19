2dba23dd7574af1b925f7f91806d73a4cec04148
Lucas Winicius <lucasclucas7@gmail.com>
2025-01-19 12:02:57 -0300

    :sparkles: feat: implementing swagger

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,62 @@
-import fastify from 'fastify';
-import 'dotenv/config';
-import routes from './routes';
-import cors from '@fastify/cors';
+import fastify from 'fastify'
+import 'dotenv/config'
+import routes from './routes'
+import cors from '@fastify/cors'
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 
-const port = parseInt(process.env.PORT || '3000');
+const port = parseInt(process.env.PORT || '3000')
 
-export const app = fastify();
+export const app = fastify()
 
-app.register(routes);
-app.register(cors, {});
+app.register(routes)
+app.register(cors, {})
+app.register(swagger, {
+  openapi: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Verbum',
+      description: 'Welcome to swagger documentation, the best way to manage your library.',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+    tags: [
+    ],
+    components: {
+      securitySchemes: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'apiKey',
+          in: 'header',
+        },
+      },
+    },
+  },
+})
+
+app.register(swaggerUi, {
+  routePrefix: '/docs', // Caminho onde a documentação estará disponível
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject) => {
+    return swaggerObject
+  },
+  transformSpecificationClone: true,
+})
 
 app.listen({ port }, (err, address) => {
   if (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   }
-  console.log(`server listening on`, address);
-});
+  console.log(`server listening on`, address)
+})
