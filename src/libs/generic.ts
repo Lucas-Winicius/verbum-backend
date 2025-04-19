272526b151eeff1e7bc47e03936eca112a413ca3
Lucas Winicius <lucasclucas7@gmail.com>
2025-04-19 15:14:41 -0300

    :bug: fix: Fixing bug in generic functions

--- src/libs/generic.ts
+++ src/libs/generic.ts
@@ -2,5 +2,17 @@ export const capitalize = (text: string) =>
   text
     .trim()
     .split(' ')
-    .map((word) => word[0].toUpperCase())
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(' ')
+
+export const title = (text: string) => {
+  const [first, ...rest] = text.trim().split(' ')
+  return [
+    first.charAt(0).toUpperCase() + first.slice(1).toLowerCase(),
+    ...rest,
+  ].join(' ')
+}
+
+console.log(title('Hello, Wold'))
+console.log(title('hello, Wold'))
+console.log(title('HELLO, WOLRD'))
